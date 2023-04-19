9ded0d46029a26fb7af72e5c517cc7d05bb804b6
acharayaP03 <acharyaP03@gmail.com>
2023-04-20 08:37:10 +1000

    Modifiy tests suits

--- src/components/__test__/ListInputFields.spec.js
+++ src/components/__test__/ListInputFields.spec.js
@@ -2,14 +2,18 @@ import { mount, shallowMount } from "@vue/test-utils";
 
 import ListInputField from '../ListInputFields.vue';
 
+describe('it checks if ListInputFields has porops', ()=> {
 
-test('uses mounts', async () => {
-  const wrapper = mount(ListInputField, {
-    props: {
-      isNewItem: false
-    }
-  });
-
-  expect(wrapper.find('button'))
+  test('uses mounts', async () => {
+    const wrapper = mount(ListInputField, {
+      props: {
+        isNewItem: false
+      }
+    });
+  
+    await wrapper.setProps( { items: [] }, { editOnIndex: false})
+  
+    expect(wrapper.props('editOnIndex')).toBe(false)
+  })
 
 })
